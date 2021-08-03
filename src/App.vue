eefddf9bdc6cc022fb160334ba7a0907119dce4b
megaton111 <lkh890111@gmail.com>
2021-08-03 11:12:40 +0900

    router 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <router-link to="/main">main</router-link>
+    <router-link to="/sub">sub</router-link>
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header, Footer
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'; 
 
 Vue.config.productionTip = false;
 
 import "@/assets/scss/_index.scss" ; 
 
 new Vue({
+  router , 
   render: h => h(App),
 }).$mount('#app')
